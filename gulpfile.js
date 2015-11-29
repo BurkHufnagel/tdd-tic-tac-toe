efaeace204379b50fddab130faaa33a7506837ab
dschinkel <dschinkel@gmail.com>
2015-11-29 02:30:18 -0600

    initial commit - config stuff

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    server = require('gulp-develop-server');
+
+var config = {
+    core: {
+        src: '*.js'
+    },
+    server: {
+        path: './game.js'
+    },
+    test:{
+        src: {
+            unit:'test/**/*-test.js'
+        },
+        mocha: {
+            reporter: 'spec'
+        }
+    }
+};
+
+gulp.task('default', ['local'], function(){
+});
+
+gulp.task('local',['mocha-unit'], function() {
+    gulp.watch('**/*.js', ['mocha-unit']);
+});
+
+gulp.task('mocha-unit', function() {
+    process.env.PORT = 8001;
+    return gulp.src([config.test.src.unit], { read: false })
+        .pipe(mocha({
+            reporter: config.test.mocha.reporter,
+            ui: 'bdd'
+        }))
+});
