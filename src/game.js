c905830abca32b45dfdf1b546ad0c9861a32e249
dschinkel <dschinkel@gmail.com>
2015-11-29 04:10:20 -0600

    decided to change start to initialize

--- src/game.js
+++ src/game.js
@@ -4,13 +4,13 @@ var board = require('./board'),
     player = require('./player');
 
 var game = module.exports = {
-    start: start,
-    started: false,
+    initialize: initialize,
+    initialized: false,
     board: board.create(),
     humanPlayer: player.create(),
     computerPlayer: player.create('player 2 (computer)')
 };
 
-function start(){
-    game.started = true;
+function initialize(){
+    game.initialized = true;
 };
--- test/game-test.js
+++ test/game-test.js
@@ -6,31 +6,23 @@ let chai = require('chai'),
 
 describe('Starting the Game', () => {
 
-    it('should be able to start the game', () => {
-        game.start();
-
-        game.started.should.be.true;
-    });
-
     it('should contain a new board to play on when game starts', () => {
-        game.start();
+        game.initialize();
 
-        game.started.should.be.true;
+        game.initialized.should.be.true;
         should.exist(game.board);
     });
 
     it('should contain a new human player without a name when game starts', () => {
-        game.start();
+        game.initialize();
 
-        game.started.should.be.true;
         should.exist(game.humanPlayer);
         should.not.exist(game.humanPlayer.name);
     });
 
     it('should contain the computer as the opponent with a default name when game starts', () => {
-        game.start();
+        game.initialize();
 
-        game.started.should.be.true;
         should.exist(game.computerPlayer);
         game.computerPlayer.name.should.equal('player 2 (computer)');
     });
