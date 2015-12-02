ac311df1ad7027ea42f2dc089ee96881c21be528
dschinkel <dschinkel@gmail.com>
2015-12-02 15:57:29 -0600

    test refactoring

--- test/game-test.js
+++ test/game-test.js
@@ -4,26 +4,26 @@ let chai = require('chai'),
     should = chai.should(),
     Game = require('../src/game'),
     player = require('../src/player'),
+
+    humanPlayer,
     newGame;
 
-describe('Game - Initial State', () => {
+beforeEach((done) => {
+    createGame();
+    humanPlayer = newGame.humanPlayer = createPlayer("Dave");
+    done();
+});
 
-    beforeEach((done) => {
-        createGame();
-        done();
-    });
+
+describe('Game - Initial State', () => {
 
     it('should contain a new board to play on', () => {
         should.exist(newGame.board);
     });
 
     it('should be able to add a human player with a name to the game', () => {
-        var player1 = player.create("Dave");
-
-        newGame.humanPlayer = player1;
-
         should.exist(newGame.humanPlayer);
-        newGame.humanPlayer.should.have.deep.property('name', player1.name);
+        newGame.humanPlayer.should.have.deep.property('name', humanPlayer.name);
     });
 
     it('should contain the computer as the opponent with a default name', () => {
@@ -39,16 +39,10 @@ describe('Game - Initial State', () => {
 
 describe('Game - Play State', () => {
 
-    beforeEach((done) => {
-        createGame();
-        done();
-    });
-
     it('should be able to make a move on the board by setting it to a specific value"', () => {
         var moveValue = "X";
 
         newGame.play();
-        newGame.humanPlayer = player.create("Dave");
         newGame.humanPlayer.moveValue = moveValue;
 
         var move1 = {
@@ -62,7 +56,7 @@ describe('Game - Play State', () => {
 
     it('should be able to keep track consecutive moves by a player', () => {
         newGame.play();
-        var humanPlayer = newGame.humanPlayer = createPlayer("Dave");
+
         var computerPlayer = newGame.humanPlayer;
 
         var moveValue1 = "X",
@@ -81,7 +75,6 @@ describe('Game - Play State', () => {
             newGame.moves[i].should.deep.equal(move);
             i++;
         }
-
         newGame.board[moves[0].coordinates.x][moves[0].coordinates.y] = moveValue1;
         newGame.board[moves[1].coordinates.x][moves[1].coordinates.y] = moveValue2;
         newGame.board[moves[2].coordinates.x][moves[2].coordinates.y] = moveValue1;
@@ -99,7 +92,7 @@ function createGame(){
 };
 
 function createPlayer(name){
-    return player.create("Dave");
+    return player.create(name);
 }
 
 function setMoveValue(player, moveValue){
